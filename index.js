f047282d723defb239febb097c740b7527f6fc03
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-27 13:31:46 +0530

    Conected Node with HTML

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var requests = require("requests");
 
 const homeFile = fs.readFileSync("home.html", "utf-8");
 
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  return temperature;
+};
+
 const server = http.createServer((req, res) => {
   if (req.url == "/") {
     requests(
@@ -12,11 +21,13 @@ const server = http.createServer((req, res) => {
       .on("data", (chunk) => {
         const objData = JSON.parse(chunk);
         const arrData = [objData];
-        console.log(arrData);
+        // console.log(arrData);
+        const realTimeData = arrData.map((val) => (replaceVal(homeFile, val))).join("");
+        res.write(realTimeData);
       })
       .on("end", (err) => {
         if (err) return console.log("connection closed due to errors", err);
-        res.end("end");
+        res.end();
       });
   } else {
     res.end("File not found");
